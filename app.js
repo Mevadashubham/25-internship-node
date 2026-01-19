1a53aa88047d4752392176380b1e049672f61496
Mevadashubham <mevadashubham@gmail.com>
2026-01-19 19:26:31 +0530

    debug: verify render is using latest code

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ import dotenv from "dotenv";
 const ordersRoute = require("./src/routes/OrderRoutes");
 //express object
 
+console.log(" BACKEND STARTED - CORS TEST ACTIVE");
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
-app.options("*", (req, res) => {
-  res.sendStatus(200);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
 });
 app.use(express.json());
 
