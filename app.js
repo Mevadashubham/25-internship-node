22f8b468dce8a16b8ee4f6e8c377266917ef9a30
Mevadashubham <mevadashubham@gmail.com>
2026-01-19 18:13:35 +0530

    Added database

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
-const express = require("express")//express
-const mongoose = require("mongoose")
-const cors = require("cors")
-const ordersRoute = require("./src/routes/OrderRoutes")
-//express object 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const express = require("express"); //express
+const mongoose = require("mongoose");
+const cors = require("cors");
+const ordersRoute = require("./src/routes/OrderRoutes");
+//express object
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 //import role routes
 
-const roleRoutes = require("./src/routes/RoleRoutes")
-app.use(roleRoutes)
+const roleRoutes = require("./src/routes/RoleRoutes");
+app.use(roleRoutes);
 
 //userRoutes
-const userRoutes = require("./src/routes/UserRoutes")
-app.use(userRoutes)
+const userRoutes = require("./src/routes/UserRoutes");
+app.use(userRoutes);
 
+const stateRoutes = require("./src/routes/StateRoutes");
+app.use("/state", stateRoutes);
 
-const stateRoutes = require("./src/routes/StateRoutes")
-app.use("/state",stateRoutes)
+const cityRoutes = require("./src/routes/CityRoutes");
+app.use("/city", cityRoutes);
 
+const areaRoutes = require("./src/routes/AreaRoutes");
+app.use("/area", areaRoutes);
 
-const cityRoutes = require("./src/routes/CityRoutes") 
-app.use("/city",cityRoutes)
+const CategoryRoutes = require("./src/routes/CategoryRoutes");
+app.use("/category", CategoryRoutes);
 
+const SubCategoriesRoutes = require("./src/routes/SubCategoryRoutes");
+app.use("/subCategory", SubCategoriesRoutes);
 
-const areaRoutes = require("./src/routes/AreaRoutes")
-app.use("/area",areaRoutes)
+const ProductRoutes = require("./src/routes/ProductRoutes");
+app.use("/product", ProductRoutes);
 
-const CategoryRoutes = require("./src/routes/CategoryRoutes")
-app.use("/category",CategoryRoutes)
+app.use("/api", ordersRoute);
 
-const SubCategoriesRoutes = require("./src/routes/SubCategoryRoutes")
-app.use("/subCategory",SubCategoriesRoutes)
-
-const ProductRoutes = require("./src/routes/ProductRoutes")
-app.use("/product", ProductRoutes)
-
-app.use('/api', ordersRoute);
-
-mongoose.connect("mongodb://127.0.0.1:27017/25_node_internship").then(()=>{
-  console.log("database connected")
-})
-
-
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error(err));
 
 //server creation
-const PORT = 3000
-app.listen(PORT,()=>{
-  console.log("server started on..",PORT)
-})
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("server started on..", PORT);
+});
